37ca2dcc4afe3ccf1ae148a22de4cf256244eb70
AP <alexeipetrea@yahoo.com>
2022-02-19 20:07:47 +0200

    finished basics started use Effect

--- react-todo-modal/src/App.js
+++ react-todo-modal/src/App.js
@@ -1,10 +1,22 @@
-import React from 'react';
-
+import React, { useState } from "react";
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
+  const [usersList, setUsersList] = useState([]);
+  const [keyId, setKeyId] = useState(0);
+
+  const addUserHandler = (uName, uAge) => {
+    setUsersList((prevUser) => {
+      setKeyId((prevUser) => ++prevUser);
+      return [...prevUser, { name: uName, age: uAge, id: keyId + uName }];
+    });
+  };
+
   return (
     <div>
-
+      <AddUser onAddUser={addUserHandler} />
+      <UsersList users={usersList} />
     </div>
   );
 }
