24d65b33d906d9d5baf242e25eeb2945de656263
AP <alexeipetrea@yahoo.com>
2022-03-10 15:35:51 +0200

    create store folder and added context api

--- react-food-order-app-personal/src/components/Layout/Header.js
+++ react-food-order-app-personal/src/components/Layout/Header.js
@@ -3,12 +3,12 @@ import mealsImage from "../assets/meals.jpg";
 import classes from "./Header.module.css";
 import HeaderCartButton from "./HeaderCartButton";
 
-function Header() {
+function Header(props) {
   return (
     <React.Fragment>
       <header className={classes.header}>
         <h1 className={classes["header-title"]}>React Meals</h1>
-        <HeaderCartButton />
+        <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes["main-image"]}>
         <img src={mealsImage} alt="a table with meals" />
