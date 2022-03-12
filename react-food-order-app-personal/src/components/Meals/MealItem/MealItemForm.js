337e1a01cbbd0d0983b49d4546881f4e92493ef3
AP <alexeipetrea@yahoo.com>
2022-03-12 14:49:22 +0200

    used refs to get entered items amunt

--- react-food-order-app-personal/src/components/Meals/MealItem/MealItemForm.js
+++ react-food-order-app-personal/src/components/Meals/MealItem/MealItemForm.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
 function MealItemForm(props) {
+  const amountInputRef = useRef();
+
+  const [amountIsValid, setAmountIsValid] = useState(true);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+    setAmountIsValid(true);
+    props.onAddToCard(enteredAmountNumber);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <Input
+        ref={amountInputRef}
         label={"Amount"}
         input={{
           id: "amount_" + props.id,
@@ -17,9 +39,9 @@ function MealItemForm(props) {
         }}
       />
       <button>+ Add</button>
+      {!amountIsValid && <p>Please enter a valid amount</p>}
     </form>
   );
 }
 
 export default MealItemForm;
-
