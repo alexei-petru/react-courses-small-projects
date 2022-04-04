9fb70f728d87e1aa436227306b4bc8c1149b1c5e
AP <alexeipetrea@yahoo.com>
2022-04-04 20:09:32 +0200

    rewriting to do from begining

--- react-todo-modal/src/App.js
+++ react-todo-modal/src/App.js
@@ -1,24 +1,7 @@
 import React, { useState } from "react";
-import AddUser from "./components/Users/AddUser";
-import UsersList from "./components/Users/UsersList";
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
-  const [keyId, setKeyId] = useState(0);
-
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUser) => {
-      setKeyId((prevUser) => ++prevUser);
-      return [...prevUser, { name: uName, age: uAge, id: keyId + uName }];
-    });
-  };
-
-  return (
-    <React.Fragment>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </React.Fragment>
-  );
+  return <div className="test">abcd</div>;
 }
 
 export default App;
