e1443af2140774a183d887680d5439bfbd885664
AP <alexeipetrea@yahoo.com>
2022-04-13 10:15:23 +0200

    finished static food order app without state

--- react-food-order-app/src/components/Meals/MealsSummary.jsx
+++ react-food-order-app/src/components/Meals/MealsSummary.jsx
@@ -4,7 +4,7 @@ import classes from "./MealsSummary.module.css";
 const MealsSummary = () => {
   return (
     <section className={classes.summary}>
-      <h2>Delicious Meals</h2>
+      <h2>Delicious Meals 138</h2>
       <p>
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit rerum
         corrupti eveniet laudantium dolor assumenda cupiditate ad nobis, dolores
