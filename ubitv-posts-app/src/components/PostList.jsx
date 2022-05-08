7acacd71a259e8616142e2eb5625bb10ef41e577
AP <alexeipetrea@yahoo.com>
2022-05-08 20:49:03 +0200

    added conditional rendering of no posts

--- ubitv-posts-app/src/components/PostList.jsx
+++ ubitv-posts-app/src/components/PostList.jsx
@@ -1,24 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import PostItem from "./PostItem";
 import classes from "./PostList.module.css";
 import "./PostListTransitionGroup.css";
 import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const PostList = ({ posts, ...props }) => {
-  const [noPostTimer, setNoPostTimer] = useState(false);
+  const [isNoPosts, setIsNoPost] = useState(false);
 
-  if (!posts.length) {
-    return <h1 className={classes.noPosts}>No added Posts</h1>;
+  const noPostsTitle = (
+    <div>
+      <h1 className={classes.noPosts}>No added Posts</h1>
+    </div>
+  );
 
-    // <div>
-    //   <CSSTransition in={noPostTimer} timeout={500} classNames="post">
-    //   </CSSTransition>
-    //   </div>
-  }
+  useEffect(() => {
+    if (!posts.length) {
+      setIsNoPost(true);
+    } else {
+      setIsNoPost(false);
+    }
+  }, [posts]);
 
   return (
     <div>
-      <h1 className={classes.title}>{props.title}</h1>
+      {isNoPosts ? (
+        noPostsTitle
+      ) : (
+        <h1 className={classes.title}>{props.title}</h1>
+      )}
       <TransitionGroup>
         {posts.map((post, index) => (
           <CSSTransition key={post.id} timeout={500} classNames={"post"}>
